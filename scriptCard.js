80ac24e5d9fbb18c8a69659f49fb05e081337c6a
Arien <Laurilin2022@gmail.com>
2023-12-10 13:29:22 +0100

    feat: перенос текста под карточку

--- scriptCard.js
+++ scriptCard.js
@@ -1,3 +1,22 @@
-const card = document.querySelector('.card')
-const cardTitle = document.querySelector('.card__title')
-const cardContacts = document.querySelector('.card__contacts')
+const card = document.querySelector('.card');
+const cardTitle = document.querySelector('.card__title');
+const cardContacts = document.querySelector('.card__contacts');
+
+// перенос текста под карточку при размере 580
+const rearrangeElement = () => {
+  const screenWidth = window.innerWidth;
+
+  if (screenWidth <= 580) {
+    card.after(cardContacts);
+  } else {
+    cardTitle.after(cardContacts);
+  }
+};
+
+
+const init = () => {
+  rearrangeElement();
+  window.addEventListener('resize', rearrangeElement);
+};
+
+init();
